1e4ae41fc2be2203ba6c24b8c470364c7de6ac51
juhar-riski <juharkades123@gmail.com>
2024-11-08 18:48:50 +0700

    repair

--- src/services/picture.js
+++ src/services/picture.js
@@ -57,7 +57,7 @@ class Gambar {
     }
 
     async deletePicture(pictureId) {
-        await prisma.picture.delete({
+        return await prisma.picture.delete({
             where: { id: Number(pictureId) },
         });
     }
